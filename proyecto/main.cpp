7f0c9f635ce2bc471f0031587f5242cd8861ffd1
Miguel Motta <miguel@MacBook-Air-de-Miguel.local>
2019-01-16 16:15:53 -0500

    mi primer commit

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -8,8 +8,71 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+/*
+
+float suma (float a, float b){
+    return a+b;
+ 
+}
+int main(){
+    
+    int a , b;
+    cout<<"Digite un numero: "; cin>> a;
+    cout<<"Digite otro numero: "; cin>> b;
+    
+    suma (a,b)
+    
     return 0;
 }
+
+ 
+ int main(){
+ int entero = 3;
+ float flotante = 3.44;
+ double mayor = 12.234234;
+ char letra = 'a';
+ cout << mayor << endl ;
+ return 0;
+ }
+ 
+ int main(){
+ int numero;
+ 
+ cout<<"Digite un numero: ";
+ cin>>numero;
+ 
+ cout << "\n El numero es : " <<numero ;
+ 
+ return 0;
+ }
+ */
+ int main(){
+ int n1,n2, suma = 0, resta = 0, multiplicacion = 0 , division = 0;
+ 
+ cout<<"Digite un numero: "; cin>> n1;cout<<endl;
+ cout<<"Digite otro numero: "; cin>> n2;
+ 
+ suma = n1 + n2;
+ resta = n1 - n2;
+ multiplicacion = n1 * n2;
+ division = n1/n2;
+ 
+ cout<<"\nLa suma es : "<<suma<<endl;
+ cout<<"La resta es : "<<resta<<endl;
+ cout<<"La multiplicacion es : "<<multiplicacion<<endl;
+ cout<<"La division es : "<<division<<endl;
+ 
+ return 0;
+ }
+/*
+ 
+int main () {
+    
+    for(int i = 0; i<10; i++){
+        cout<< "hola we xd"<<endl;
+    }
+    
+    return 0;
+}
+*/
